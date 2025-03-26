8cf346765eb696fc9740eafa1d2a94a20baa6913
kenny2125 <johnkennyreyes21@gmail.com>
2025-03-26 13:24:23 +0800

    Revert "Increased log period so it wont flood up my whole terminal again"
    This reverts commit 73b1dab8c01a5fb2f0344f1e45b07a2c87aca93c.

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -199,7 +199,7 @@ router.get('/predict', async (req, res) => {
       iterations,
       errorThresh: 0.005,
       log: true,
-      logPeriod: 10000
+      logPeriod: 100
     };
 
     console.log(`Training GRU Time Step model with ${salesData.length} data points...`);
