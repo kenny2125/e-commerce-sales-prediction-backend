e40a92af5bcd53f5e7c0e1881a0deb030e8b8bf8
kenny2125 <johnkennyreyes21@gmail.com>
2025-05-20 22:37:20 +0800

    temp fix for new database

--- db/db.js
+++ db/db.js
@@ -9,5 +9,13 @@ const pool = new Pool({
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  query: (text, params) => {
+    // Modify SQL queries to automatically add public schema to table names
+    // This regex looks for table names that aren't already prefixed with a schema
+    const modifiedText = text.replace(
+      /(?<!(public|schema)\.)\b(tbl_users|products|product_variants|cart_items|orders|order_items|historical_sales)(?=\s|\)|;|,|$)/gi, 
+      'public.$2'
+    );
+    return pool.query(modifiedText, params);
+  },
 };
